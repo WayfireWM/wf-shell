4300eea7f89aae4719b6cebff35e40744403fab3
Ilia Bozhinov <ammen99@gmail.com>
2019-08-11 19:45:43 +0300

    background: port to layer-shell

--- src/background/background.hpp
+++ src/background/background.hpp
@@ -1,5 +1,12 @@
 #pragma once
 
+#include <glibmm/refptr.h>
+#include <config.hpp>
+#include <animation.hpp>
+#include <gtkmm/drawingarea.h>
+#include <gtkmm/window.h>
+#include <wf-shell-app.hpp>
+
 class WayfireBackground;
 
 class BackgroundImage
@@ -20,11 +27,12 @@ class BackgroundDrawingArea : public Gtk::DrawingArea
      * are used as offsets when preserve aspect is set. */
     BackgroundImage to_image, from_image;
 
-    public:
+  public:
     BackgroundDrawingArea();
-    void show_image(Glib::RefPtr<Gdk::Pixbuf> image, double offset_x, double offset_y);
+    void show_image(Glib::RefPtr<Gdk::Pixbuf> image,
+        double offset_x, double offset_y);
 
-    protected:
+  protected:
     bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
 };
 
@@ -32,16 +40,15 @@ class WayfireBackground
 {
     WayfireShellApp *app;
     WayfireOutput *output;
-    zwf_wm_surface_v1 *wm_surface = NULL;
 
     BackgroundDrawingArea drawing_area;
     std::vector<std::string> images;
     Gtk::Window window;
+
     int scale;
     double offset_x, offset_y;
-    bool inhibited = true;
+    bool inhibited = false;
     uint current_background;
-    int output_width, output_height;
     sigc::connection change_bg_conn;
 
     wf_option background_image, background_cycle_timeout,
@@ -49,8 +56,6 @@ class WayfireBackground
     wf_option_callback init_background, cycle_timeout_updated;
 
     Glib::RefPtr<Gdk::Pixbuf> create_from_file_safe(std::string path);
-    void create_wm_surface();
-    void handle_output_resize(uint32_t width, uint32_t height);
     bool background_transition_frame(int timer);
     bool change_background(int timer);
     bool load_images_from_dir(std::string path);
@@ -58,9 +63,10 @@ class WayfireBackground
     void reset_background();
     void set_background();
     void reset_cycle_timeout();
+
     void setup_window();
 
-    public:
+  public:
     WayfireBackground(WayfireShellApp *app, WayfireOutput *output);
     ~WayfireBackground();
 };
