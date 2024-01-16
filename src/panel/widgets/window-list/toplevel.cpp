1a4789a0f1d92dd0092564906713de8bc50b2431
Carter Prince <carteraprince@gmail.com>
2024-01-16 00:10:24 -0500

    fix spacing, remove tab

--- src/panel/widgets/window-list/toplevel.cpp
+++ src/panel/widgets/window-list/toplevel.cpp
@@ -188,7 +188,7 @@ class WayfireToplevel::impl
             return true; // It has been handled.
         } else if ((event->type == GDK_BUTTON_PRESS) && (event->button == 2)) // middle click
         {
-	    zwlr_foreign_toplevel_handle_v1_close(handle);
+            zwlr_foreign_toplevel_handle_v1_close(handle);
             return true;
         } else
         {
