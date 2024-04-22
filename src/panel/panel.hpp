5e6898de29a8c6a3a8b02ff564c9ca4352e87de8
trigg <triggerhapp@googlemail.com>
2024-04-22 08:35:52 +0100

    wf-panel: hot-reload css (#240)
    * wf-shell-app: create user css directory
    wf-shell-app: watch user css directory for changes
    wf-panel: on css file change load changes to css
    
    * uncrustify
    
    * uncrustify
    
    * panel: populate default css on first run
    panel: build default css to prefix
    
    * uncrustify

--- src/panel/panel.hpp
+++ src/panel/panel.hpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <wayland-client.h>
 #include <gtkmm/window.h>
+#include <gtkmm/cssprovider.h>
 
 #include "wf-shell-app.hpp"
 
@@ -35,11 +36,16 @@ class WayfirePanelApp : public WayfireShellApp
     void handle_new_output(WayfireOutput *output) override;
     void handle_output_removed(WayfireOutput *output) override;
     void on_config_reload() override;
+    void on_css_reload() override;
 
   private:
     WayfirePanelApp(int argc, char **argv);
 
+    void clear_css_rules();
+    void add_css_file(std::string file);
+
     class impl;
+    std::vector<Glib::RefPtr<Gtk::CssProvider>> css_rules;
     std::unique_ptr<impl> priv;
 };
 
