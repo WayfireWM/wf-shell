69279c9ff850775a156d6cc803b2c81abbd811f0
Hue <skeptikal.monke@gmail.com>
2025-12-21 02:13:29 +0100

    new light widget

---
+++ src/panel/widgets/light/light.hpp
@@ -0,0 +1,42 @@
+/*
+  abstract light control class, which are handled by a widget
+  other .cpp files in this directory are backends for different ways of handling them
+*/
+
+#include <gtkmm.h>
+
+#include "widget.hpp"
+#include "animated-scale.hpp"
+
+class WfLightControl : public Gtk::Box
+{
+  protected:
+    WayfireAnimatedScale scale;
+    Gtk::Label label;
+
+    virtual std::string get_name();
+
+  public:
+    WfLightControl();
+
+    // a double from 0 to 1 for min to max
+    virtual void set_brightness(double brightness);
+    virtual double get_brightness();
+};
+
+class WayfireLight : public WayfireWidget {
+  private:
+    void init(Gtk::Box *container) override;
+    Gtk::Image icon;
+
+    std::unique_ptr<WayfireMenuButton> button;
+    Gtk::Popover *popover;
+    Gtk::Box box;
+
+    std::vector<std::unique_ptr<WfLightControl>> controls;
+
+    WfOption<double> scroll_sensitivity{"panel/wp_scroll_sensitivity"};
+
+  public:
+    void update_icon();
+};
