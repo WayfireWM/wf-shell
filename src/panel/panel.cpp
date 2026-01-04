4c924ba1b01e22dfc32fde2f5a26239b36a4d3a9
Hue <skeptikal.monke@gmail.com>
2026-01-04 17:13:05 +0100

    light icon size config

--- src/panel/panel.cpp
+++ src/panel/panel.cpp
@@ -401,6 +401,7 @@ void WayfirePanelApp::on_activate()
     new CssFromConfigString("panel/background_color", ".wf-panel{background-color:", ";}");
     new CssFromConfigBool("panel/battery_icon_invert", ".battery image{filter:invert(100%);}", "");
     new CssFromConfigBool("panel/network_icon_invert_color", ".network-icon{filter:invert(100%);}", "");
+    new CssFromConfigInt("panel/light_icon_size", ".light{-gtk-icon-size:", "px;}");
 
     new CssFromConfigFont("panel/battery_font", ".battery {", "}");
     new CssFromConfigFont("panel/clock_font", ".clock {", "}");
