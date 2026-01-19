e68b04e66402604bee046859b0d126468b377107
trigg <triggerhapp@gmail.com>
2026-01-19 01:05:15 +0000

    - uncrustify

--- src/locker/plugin/volume.hpp
+++ src/locker/plugin/volume.hpp
@@ -15,12 +15,13 @@ class WayfireLockerVolumePlugin : public WayfireLockerPlugin
 {
   private:
     GvcMixerControl *gvc_control;
-    GvcMixerStream *gvc_sink_stream = NULL;
+    GvcMixerStream *gvc_sink_stream   = NULL;
     GvcMixerStream *gvc_source_stream = NULL;
-    gulong notify_sink_muted_signal = 0;
+    gulong notify_sink_muted_signal   = 0;
     gulong notify_source_muted_signal = 0;
     void disconnect_gvc_stream_sink_signals();
     void disconnect_gvc_stream_source_signals();
+
   public:
     WayfireLockerVolumePlugin();
     void add_output(int id, Gtk::Grid *grid) override;
@@ -36,7 +37,6 @@ class WayfireLockerVolumePlugin : public WayfireLockerPlugin
 
     std::unordered_map<int, std::shared_ptr<Gtk::Button>> sink_buttons;
     std::unordered_map<int, std::shared_ptr<Gtk::Button>> source_buttons;
-    
 };
 
 #endif
