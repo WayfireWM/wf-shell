b404dbbe47c72603b4742eb4b7aa4822fb067916
trigg <triggerhapp@gmail.com>
2026-01-19 00:46:49 +0000

    - Improved (?) dbus error catching - Delay and Retry claiming finger print reader on failure -  Remove saving and using of name of fingerprint to request as we weren't using it anyway - Added volume mute & mic mute plugin

---
+++ src/locker/plugin/volume.hpp
@@ -0,0 +1,42 @@
+#ifndef LOCKER_VOLUME_PLUGIN_HPP
+#define LOCKER_VOLUME_PLUGIN_HPP
+
+#include <gtkmm/button.h>
+#include <gtkmm/popover.h>
+#include <gtkmm/scale.h>
+#include <memory>
+#include <unordered_map>
+#include <pulse/pulseaudio.h>
+#include "gvc-mixer-control.h"
+
+#include "../plugin.hpp"
+
+class WayfireLockerVolumePlugin : public WayfireLockerPlugin
+{
+  private:
+    GvcMixerControl *gvc_control;
+    GvcMixerStream *gvc_sink_stream = NULL;
+    GvcMixerStream *gvc_source_stream = NULL;
+    gulong notify_sink_muted_signal = 0;
+    gulong notify_source_muted_signal = 0;
+    void disconnect_gvc_stream_sink_signals();
+    void disconnect_gvc_stream_source_signals();
+  public:
+    WayfireLockerVolumePlugin();
+    void add_output(int id, Gtk::Grid *grid) override;
+    void remove_output(int id) override;
+    bool should_enable() override;
+    void init() override;
+    bool enable;
+    void update_button_images();
+
+    /** Called when the default sink changes */
+    void on_default_sink_changed();
+    void on_default_source_changed();
+
+    std::unordered_map<int, std::shared_ptr<Gtk::Button>> sink_buttons;
+    std::unordered_map<int, std::shared_ptr<Gtk::Button>> source_buttons;
+    
+};
+
+#endif
