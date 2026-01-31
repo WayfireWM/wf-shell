bb33b8ec8f16503d96f9c5e0f1a182f578cd38da
Hue <skeptikal.monke@gmail.com>
2026-01-31 18:01:02 +0100

    more ddcutil core

--- src/panel/widgets/light/ddcutil.cpp
+++ src/panel/widgets/light/ddcutil.cpp
@@ -5,25 +5,78 @@ extern "C"{
 
 #include "light.hpp"
 
+#define VCP_BRIGHTNESS_CODE 10
+
 class WfLightDdcaControl : public WfLightControl
 {
-	friend class DdcaSurveillor;
-
 	private:
-		DDCA_Display_Ref ref;
-};
+		DDCA_Display_Handle handle;
+		int max;
 
-class DdcaSurveillor {
-	private:
-		DdcaSurveillor(){
-			ddca_enable_verify(true);
-			DDCA_Display_Info_List *display_list = NULL;
-			ddca_get_display_info_list2(false, &display_list);
-
-			for (int i = 0 ; i < display_list->ct ; i++){
-				display_refs.push_back(display_list[i]);
-			}
+		int get_max(){
+			return max;
 		}
 
-		std::vector<DDCA_Display_Info> display_refs;
+	public:
+		WfLightDdcaControl(WayfireLight *parent, DDCA_Display_Ref ref) : WfLightControl(parent){
+			DDCA_Status status = ddca_open_display2(ref, false, &handle);
+
+			DDCA_Non_Table_Vcp_Value value;
+			status = ddca_get_non_table_vcp_value(handle, VCP_BRIGHTNESS_CODE, &value);
+			max = value.mh << 8 | value.ml;
+		}
+
+		~WfLightDdcaControl(){
+			ddca_close_display(handle);
+		}
+
+		std::string get_name(){
+			std::string name;
+			name = "display";
+			return name;
+		}
+
+        void set_brightness(double brightness){
+			uint16_t value = (uint16_t)(get_max() * brightness);
+			uint8_t sh = value >> 8;
+			uint8_t sl = value & 0xFF;
+			DDCA_Status status = ddca_set_non_table_vcp_value(handle, VCP_BRIGHTNESS_CODE, sh, sl);
+        }
+
+        double get_brightness(){
+			DDCA_Non_Table_Vcp_Value value;
+			DDCA_Status status = ddca_get_non_table_vcp_value(handle, VCP_BRIGHTNESS_CODE, &value);
+			return value.sh << 8 | value.sl;
+	    }
+};
+
+DdcaSurveillor::DdcaSurveillor(){
+	ddca_enable_verify(true);
+	DDCA_Display_Info_List *display_list = NULL;
+	ddca_get_display_info_list2(false, &display_list);
+
+	for (int i = 0 ; i < display_list->ct ; i++){
+		displays_info.push_back(&display_list->info[i]);
+	}
+}
+
+void DdcaSurveillor::catch_up_widget(WayfireLight *widget){
+    for (auto info : displays_info){
+        auto control = std::make_shared<WfLightDdcaControl>(widget, info->dref);
+        // it.second.second.push_back(std::shared_ptr<WfLightSysfsControl>(control));
+        widget->add_control((std::shared_ptr<WfLightControl>)control);
+    }
+
+}
+
+void DdcaSurveillor::strip_widget(WayfireLight *widget){
+
+}
+
+DdcaSurveillor& DdcaSurveillor::get(){
+    if (!instance)
+    {
+        instance = std::unique_ptr<DdcaSurveillor>(new DdcaSurveillor());
+    }
+    return *instance;
 }
