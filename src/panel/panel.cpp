dd8eee8cc1a2264d971afb626fbb571a41673367
Scott Moreau <oreaus@gmail.com>
2026-02-19 14:38:13 -0700

    weather: Rebase on master

--- src/panel/panel.cpp
+++ src/panel/panel.cpp
@@ -19,7 +19,9 @@
 #include "widgets/language.hpp"
 #include "widgets/menu.hpp"
 #include "widgets/clock.hpp"
+#ifdef HAVE_WEATHER
 #include "widgets/weather.hpp"
+#endif
 #include "widgets/launchers.hpp"
 #include "widgets/network.hpp"
 #include "widgets/spacing.hpp"
@@ -149,12 +151,12 @@ class WayfirePanel::impl
         {
             return Widget(new WayfireClock());
         }
-
+#ifdef HAVE_WEATHER
         if (name == "weather")
         {
             return Widget(new WayfireWeather());
         }
-
+#endif
         if (name == "network")
         {
             return Widget(new WayfireNetworkInfo());
