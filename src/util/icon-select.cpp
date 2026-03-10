3f2cf9f82cd93b3f6f90a87b2345d9c27e6631d5
Hue <skeptikal.monke@gmail.com>
2026-03-10 14:58:44 +0100

    utils : icons : introduce multiple icon possibilities with priorities
    icon tables can now have multiple choices for an icon and fall back if the
    desired one is not found. For proper function, requires the last choice to be
    a standard icon.

--- src/util/icon-select.cpp
+++ src/util/icon-select.cpp
@@ -1,12 +1,28 @@
+#include <gdkmm/display.h>
+#include <gtkmm/icontheme.h>
+
 #include "icon-select.hpp"
 
-std::string icon_from_range(std::map<double, std::string> icons, double value)
+std::string icon_from_range(std::map<double, std::vector<std::string>> icons, double value)
 {
     for (auto pair : icons)
     {
         if (value <= pair.first)
         {
-            return pair.second;
+            auto theme = Gtk::IconTheme::get_for_display(Gdk::Display::get_default());
+            if (!theme)
+            {
+                // let’s stick to standard icons in this case
+                return *pair.second.end();
+            }
+
+            for (auto name : pair.second)
+            {
+                if (theme->has_icon(name))
+                {
+                    return name;
+                }
+            }
         }
     }
 
