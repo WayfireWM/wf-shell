b305e9b910656be1cc8580e0dbe16bcbf1ed6206
mark.herbert <mark.herbert42@gmail.com>
2026-05-19 11:35:30 +0300

    Separate animated scale and icon select from volume.cpp to utils

--- src/panel/widgets/volume.cpp
+++ src/panel/widgets/volume.cpp
@@ -4,46 +4,9 @@
 #include "launchers.hpp"
 #include "gtk-utils.hpp"
 #include "panel.hpp"
+#include "icon-select.hpp"
 
-WayfireVolumeScale::WayfireVolumeScale()
-{
-    this->signal_draw().connect_notify(
-        [=] (const Cairo::RefPtr<Cairo::Context>& ctx)
-    {
-        if (this->current_volume.running())
-        {
-            value_changed.block();
-            this->set_value(this->current_volume);
-            value_changed.unblock();
-        }
-    }, true);
-
-    value_changed = this->signal_value_changed().connect_notify([=] ()
-    {
-        this->current_volume.animate(this->get_value(), this->get_value());
-        if (this->user_changed_callback)
-        {
-            this->user_changed_callback();
-        }
-    });
-}
-
-void WayfireVolumeScale::set_target_value(double value)
-{
-    this->current_volume.animate(value);
-    this->queue_draw();
-}
-
-double WayfireVolumeScale::get_target_value() const
-{
-    return this->current_volume.end;
-}
-
-void WayfireVolumeScale::set_user_changed_callback(
-    std::function<void()> callback)
-{
-    this->user_changed_callback = callback;
-}
+#define ICON(volume) icon_from_range(volume_icons, volume)
 
 enum VolumeLevel
 {
---
+++ src/util/icon-select.hpp
@@ -0,0 +1,29 @@
+#include <map>
+#include <vector>
+#include <string>
+#include <limits>
+
+std::string icon_from_range(std::map<double, std::vector<std::string>> icons, double value);
+
+// the number in the first term is the maximal value at which this icon will be shown
+// selection values of the tables are expected to be ordered from least to greatest
+// the vector of icons is the different possibilites of icons to show,
+// with the prefered one as the first. This is to be able to use non-standard icons
+// if they are present in the current theme, and fall back to a standard one if not.
+
+const std::map<double, std::vector<std::string>> volume_icons = {
+    {std::numeric_limits<double>::min(), {"emblem-unreadable"}},
+    {0.0, {"audio-volume-muted"}},
+    {0.33, {"audio-volume-low"}},
+    {0.66, {"audio-volume-medium"}},
+    {1.0, {"audio-volume-high"}},
+    {std::numeric_limits<double>::max(), {"audio-volume-high-danger", "dialog-warning"}}
+};
+
+const std::map<double, std::vector<std::string>> brightness_display_icons = {
+    {std::numeric_limits<double>::min(), {"display-brightness-invalid", "emblem-unreadable"}},
+    {0, {"display-brightness-low-symbolic", "display-brightness-low"}},
+    {0.5, {"display-brightness-medium-symbolic", "display-medium-low"}},
+    {1, {"display-brightness-high-symbolic", "display-brightness-high"}},
+    {std::numeric_limits<double>::max(), {"display-brightness-invalid", "emblem-unreadable"}},
+};
