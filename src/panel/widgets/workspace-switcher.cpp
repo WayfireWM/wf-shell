504edb550f6174a84f4c0022999ca467c285129d
Hue <skeptikal.monke@gmail.com>
2026-08-02 19:58:22 +0200

    panel : switcher : fix broken row mode
    The row mode was unable to work good because the output dimensions were never
    actually set, just declaring a local variable instead of setting the member.

--- src/panel/widgets/workspace-switcher.cpp
+++ src/panel/widgets/workspace-switcher.cpp
@@ -423,8 +423,8 @@ void WayfireWorkspaceSwitcher::process_workspaces(wf::json_t workspace_data)
             if (this->output_name == output_data["name"].as_string())
             {
                 auto output_id     = output_data["id"].as_int();
-                auto output_width  = output_data["geometry"]["width"].as_int();
-                auto output_height = output_data["geometry"]["height"].as_int();
+                this->output_width = output_data["geometry"]["width"].as_int();
+                this->output_height = output_data["geometry"]["height"].as_int();
                 for (auto w : windows)
                 {
                     if (w->active)
